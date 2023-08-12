071c1f149ee2cbc5e0dd8d619b65d5e127875357
Alexandria Webb <alexandriakwebb@gmail.com>
2023-08-12 09:07:12 -0600

    Adds code to pass all tests.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Alex is the champion"
+document.body.append(newHeader);
+
+
+
